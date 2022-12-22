74f6be5f0c2112cd8285bf0db7c1f49c1e58c7b7
utkubarancavus <utkubarancavus@icloud.com>
2022-12-22 13:57:11 -0600

    yeni

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+    public static final String Configuration_FilePath  = System.getProperty("user.dir") + "/src/test/resources/config/config.properties";
+
+    public static final int Implicit_Wait = 10;
+    public static final int Explicit_wait = 20;
+
+    public static final String ScreenShot_FilePath = System.getProperty("user.dir")+"/screenshots/";
+
+}
---
+++ src/test/java/utils/commonMethods.java
@@ -0,0 +1,88 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class commonMethods  {
+    public static WebDriver driver;
+
+
+    public static void openAndLaunchApplication(){
+
+        ConfigReader.readProperties(Constants.Configuration_FilePath);
+
+        switch (ConfigReader.getPropertyValues("browser")){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("Invalid browser name");
+        }
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getPropertyValues("url"));
+        driver.manage().timeouts().implicitlyWait(Constants.Implicit_Wait, TimeUnit.SECONDS);
+
+        //this method is used to initialize all the objects of the pages
+        //initializePageObjects();
+
+    }
+
+    public static void closeBrowser(){
+        driver.quit();
+    }
+
+    public static String getTimeStamp(String pattern){
+        Date date = new Date();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        return simpleDateFormat.format(date);
+    }
+
+    public static byte[] takeScreenShot(String fileName) throws IOException {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+
+        byte[] picBytes = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+        File sourceFile =takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(sourceFile, new File(Constants.ScreenShot_FilePath + fileName + " " + getTimeStamp("yyyy-MM-dd-HH-mm-ss")+".png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return picBytes;
+
+    }
+    public static WebDriverWait getWait() {
+        WebDriverWait wait = new WebDriverWait(driver,Constants.Explicit_wait);
+        return wait;
+    }
+    public static void waitForClickable(WebElement element){
+        getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void click(WebElement element){
+        waitForClickable(element);
+        element.click();
+    }
+
+
+}
